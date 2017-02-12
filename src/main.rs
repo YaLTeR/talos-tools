4b52c476e05e3b3ec11ed07045ffb8a91d7b217f
Ivan Molodetskikh <yalterz@gmail.com>
2017-02-12 15:57:04 +0300

    Make everything not terrible
    Rewrite the main loop. Now Escape only works in the Talos window, and
    now the random crashes should be gone.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
-#![macro_use]
-extern crate lazy_static;
 extern crate libc;
 extern crate x11;
 
-use std::ffi::CString;
-use std::{mem, ptr};
+use std::ffi::CStr;
+use std::{mem, ptr, thread, time};
 use x11::keysym;
 use x11::xlib::*;
 
 struct XlibStuff {
     pub display: *mut Display,
     pub root: Window,
-    pub net_active_window: Atom,
     _h: ()
 }
 
@@ -26,53 +23,11 @@ impl XlibStuff {
             let screen = XDefaultScreen(display);
             let root = XRootWindow(display, screen);
 
-            let net_active_window = XInternAtom(display,
-                                                CString::new("_NET_ACTIVE_WINDOW").unwrap().as_ptr(),
-                                                True);
-
             Ok(XlibStuff {
                 display: display,
                 root: root,
-                net_active_window: net_active_window,
                 _h: ()
             })
-        }
-    }
-
-    fn get_active_window(&self) -> Result<Option<Window>, String> {
-        unsafe {
-            let mut actual_type_return = mem::uninitialized::<Atom>();
-            let mut actual_format_return = mem::uninitialized::<libc::c_int>();
-            let mut nitems_return = mem::uninitialized::<libc::c_ulong>();
-            let mut bytes_after_return = mem::uninitialized::<libc::c_ulong>();
-            let mut data = mem::uninitialized::<*mut libc::c_uchar>();
-            if XGetWindowProperty(self.display,
-                                  self.root,
-                                  self.net_active_window,
-                                  0,
-                                  !0,
-                                  False,
-                                  AnyPropertyType as u64,
-                                  &mut actual_type_return,
-                                  &mut actual_format_return,
-                                  &mut nitems_return,
-                                  &mut bytes_after_return,
-                                  &mut data) != Success as i32 {
-                return Err("XGetWindowProperty() did not return Success.".to_owned());
-            }
-
-            if data.is_null() {
-                return Err("XGetWindowProperty() returned NULL as data.".to_owned());
-            }
-
-            let active_window = *(data as *mut Window);
-
-            XFree(data as *mut std::os::raw::c_void);
-
-            match active_window {
-                0 => Ok(None),
-                w => Ok(Some(w)),
-            }
         }
     }
 
@@ -82,10 +37,6 @@ impl XlibStuff {
             if XGetWindowAttributes(self.display, window, &mut window_attributes) == 0 {
                 return Err("XGetWindowAttributes() returned 0.".to_owned());
             }
-
-            //println!("Window w, h: {} {}",
-            //         window_attributes.width,
-            //         window_attributes.height);
 
             XWarpPointer(self.display,
                          window,
@@ -100,6 +51,49 @@ impl XlibStuff {
 
         Ok(())
     }
+
+    fn get_window_by_name(&self, name: &str, current: Window) -> Option<Window> {
+        unsafe {
+            let mut window_name = mem::uninitialized::<*mut libc::c_char>();
+
+            if XFetchName(self.display, current, &mut window_name) != 0 {
+                if let Ok(window_name_str) = CStr::from_ptr(window_name).to_str() {
+                    if window_name_str == "Talos - Linux" {
+                        XFree(window_name as *mut std::os::raw::c_void);
+
+                        return Some(current);
+                    }
+                }
+
+                XFree(window_name as *mut std::os::raw::c_void);
+            }
+
+            let mut root_return = mem::uninitialized::<Window>();
+            let mut parent_return = mem::uninitialized::<Window>();
+            let mut children = mem::uninitialized::<*mut Window>();
+            let mut children_count = mem::uninitialized::<libc::c_uint>();
+
+            if XQueryTree(self.display,
+                          current,
+                          &mut root_return,
+                          &mut parent_return,
+                          &mut children,
+                          &mut children_count) != 0 {
+                for i in 0..children_count {
+                    if let Some(window) = self.get_window_by_name(name,
+                                                                  *children.offset(i as isize)) {
+                        XFree(children as *mut std::os::raw::c_void);
+
+                        return Some(window);
+                    }
+                }
+
+                XFree(children as *mut std::os::raw::c_void);
+            }
+
+            None
+        }
+    }
 }
 
 impl Drop for XlibStuff {
@@ -110,83 +104,39 @@ impl Drop for XlibStuff {
     }
 }
 
-fn warp_cursor(x: &XlibStuff) -> Result<(), String> {
-    let active_window = match try!(x.get_active_window()) {
-        Some(w) => w,
-        None => return Ok(()),
-    };
-
-    try!(x.warp_cursor_to_center(active_window));
-
-    Ok(())
-}
-
-fn run_event_loop(x: &XlibStuff) -> Result<(), String> {
+fn run_event_loop(x: &XlibStuff) {
     unsafe {
-        // This approach works but it blocks programs from using Escape for the most part.
-        // Most annoyingly, it blocks usage of Escape in the Talos menu.
-        // Also it doesn't work with modifiers (you have to have NumLock off for example).
-
-        //if XGrabKey(x.display,
-        //            XKeysymToKeycode(x.display, keysym::XK_Escape as u64) as i32,
-        //            0,
-        //            x.root,
-        //            False,
-        //            GrabModeAsync,
-        //            GrabModeAsync) == BadAccess as i32 {
-        //    return Err("XGrabKey() returned BadAccess.".to_owned());
-        //}
-
-        //XSelectInput(x.display, x.root, KeyPressMask);
-
-        //loop {
-        //    let mut ev = mem::uninitialized::<XEvent>();
-        //    XNextEvent(x.display, &mut ev);
-
-        //    match ev.get_type() {
-        //        t if t == KeyPress => {
-        //            try!(warp_cursor(&x));
-        //        },
-        //        _ => {}
-        //    };
-        //}
-
-        // This sometimes gives BadWindow?
-        // Also doesn't work with all windows (but does work with Talos).
-
-        let mut current_focus_window = mem::uninitialized::<Window>();
-        let mut revert_to_return = mem::uninitialized::<libc::c_int>();
-        XGetInputFocus(x.display, &mut current_focus_window, &mut revert_to_return);
-        XSelectInput(x.display, current_focus_window, KeyPressMask | FocusChangeMask);
-
         loop {
-            let mut ev = mem::uninitialized::<XEvent>();
-            XNextEvent(x.display, &mut ev);
+            if let Some(talos_window) = x.get_window_by_name("Talos - Linux", x.root) {
+                println!("Found the Talos window.");
 
-            match ev.get_type() {
-                t if t == FocusOut => {
-                    if current_focus_window != x.root {
-                        XSelectInput(x.display, current_focus_window, 0);
+                XSelectInput(x.display, talos_window, KeyPressMask | StructureNotifyMask);
+                loop {
+                    let mut ev = mem::uninitialized::<XEvent>();
+                    XNextEvent(x.display, &mut ev);
+
+                    match ev.get_type() {
+                        t if t == KeyPress => {
+                            if XKeyEvent::from(ev).keycode == XKeysymToKeycode(x.display,
+                                                                               keysym::XK_Escape as u64) as u32 {
+                                if x.warp_cursor_to_center(talos_window).is_err() {
+                                    break;
+                                }
+                            }
+                        }
+
+                        t if t == DestroyNotify => {
+                            println!("The Talos window was closed.");
+                            break;
+                        }
+
+                        _ => {}
                     }
+                }
+            }
 
-                    XGetInputFocus(x.display, &mut current_focus_window, &mut revert_to_return);
-
-                    if current_focus_window == PointerRoot as u64 {
-                        current_focus_window = x.root;
-                    }
-
-                    XSelectInput(x.display, current_focus_window, KeyPressMask | FocusChangeMask);
-                },
-
-                t if t == KeyPress => {
-                    if XKeyEvent::from(ev).keycode == XKeysymToKeycode(x.display,
-                                                                       keysym::XK_Escape as u64) as u32 {
-                        try!(warp_cursor(&x));
-                    }
-                },
-
-                _ => {}
-            };
+            // Wait for Talos to be opened.
+            thread::sleep(time::Duration::from_secs(1));
         }
     }
 }
@@ -200,7 +150,5 @@ fn main() {
         }
     };
 
-    if let Err(err) = run_event_loop(&x) {
-        println!("{}", err);
-    }
+    run_event_loop(&x);
 }
